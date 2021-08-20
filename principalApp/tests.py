f1ceb1505735002a3519e40d68620b2ec1624cc0
Ever Fernando Garay Molinas <everfgmolinas@fpuna.edu.py>
2021-08-20 08:27:04 -0400

    Cambio en conexion a postgres arreglado

--- principalApp/tests.py
+++ principalApp/tests.py
@@ -1,28 +1,3 @@
 from django.test import TestCase
-from Proyecto.wsgi import *
-from principalApp.models import *
-
-import datetime
-"""
-# creating an instance of
-# datetime.date
-d = datetime.date(1997, 10, 14) # %y/$m/%d
-# Insertar datos
-
-p1 = Usuario()
-p1.names = 'Usuario2'
-p1.ci = '1111116'
-p1.fecha_nac = d
-p1.lastname = 'Test'
-
-#p1.save()
-
-# Visualizar datos
-print(Usuario.objects.all())
 
 # Create your tests here.
-
-pr1 = Proyecto()
-pr1.name = 'Proyecto01'
-pr1.creator = 1
-"""
