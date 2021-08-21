7e465011b7d0bbb558ea91c90b264502f9146116
Ever Fernando Garay Molinas <everfgmolinas@fpuna.edu.py>
2021-08-20 20:34:30 -0400

    Rebase y prueba

--- principalApp/tests.py
+++ principalApp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from Proyecto.wsgi import *
+from principalApp.models import Usuario
+
+import datetime
+
+# creating an instance of
+# datetime.date
+d = datetime.date(1997, 10, 14) # %y/$m/%d
+# Insertar datos
+
+p1 = Usuario()
+p1.names = 'Usuario2'
+p1.ci = '1111116'
+p1.fecha_nac = d
+p1.lastname = 'Test'
+
+p1.save()
+
+# Visualizar datos
+print(Usuario.objects.all())
 
 # Create your tests here.
