54bae33b9e944debbf2ae57a3a620c7305f18ee1
elias <eliascaceres124@fpuna.edu.py>
2021-08-24 22:29:48 -0400

    nuevo html+css+js base

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -11,12 +11,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+
 from django.urls import reverse_lazy
 import Proyecto.db as db
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 
 
 # Quick-start development settings - unsuitable for production
