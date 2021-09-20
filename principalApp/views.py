96cbc37afdaf7a281f8ef2b1e4b12c2fcafc9b0b
franpintos <pintos98javier99@fpuna.edu.py>
2021-09-20 01:08:34 -0400

    Permisos para vistas agregados

--- principalApp/views.py
+++ principalApp/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View, TemplateView
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-class inicioview(TemplateView):
-    template_name = 'inicio.html'
+@login_required(login_url='login')
+def inicioview(request):
+    user = request.user
+    perm_proyecto_ver = user.has_perm('project_view')
+    perm_roles_ver = user.has_perm('roles_view')
+    context = {
+        'perm_proyectos_ver': perm_proyecto_ver,
+        'perm_roles_ver': perm_roles_ver,
+    }
+    return render(request, 'inicio.html', context=context)
 
-
-class baseview(TemplateView):
-    template_name = 'base.html'
+def baseview(request):
+    return render(request, 'base.html', context=context)
 
 class calendar(TemplateView):
         template_name = 'calendar.html'
-
-
-# def inico(request):
-#     return render(request,"principalApp/inicio.html")
-#
-# def base(request):
-#     return render(request,"principalApp/base.html")
 
 def pr(request):
     return render(request,"principalApp/pr.html")
