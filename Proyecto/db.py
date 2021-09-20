b6fa07a3e0fcc440fd2d5c7337430174a0331729
everfgmolinasFPUNA <everfgmolinas@fpuna.edu.py>
2021-09-20 13:48:40 -0400

    Commit para tag V3.0

--- Proyecto/db.py
+++ Proyecto/db.py
@@ -15,7 +15,7 @@ SQLITE = {
 POSTGRESQL = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'desarrollo',
+        'NAME': 'produccion',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
