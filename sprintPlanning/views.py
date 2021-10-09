43a86720a70a6ddd37ba3cd598d6f153ebb60621
lucascandia <lucascandiasolis@fpuna.edu.py>
2021-10-09 04:20:21 -0300

    Sprint Plannint etapa 4

--- sprintPlanning/views.py
+++ sprintPlanning/views.py
@@ -1,11 +1,16 @@
 import datetime
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+
+from equipo.models import Equipo
 from miembros.models import Miembro
 from project.models import Proyecto
 from sprint.models import Sprint
-from sprintPlanning.forms import primerpasoplanificarSprint
+from sprintPlanning.forms import primerpasoplanificarSprint, tercerpasoplanificarSprint, PlanificarUs
+from us.models import Us
 
 
 def modificar_sprintplanni(request, pk, sp_pk):
@@ -67,7 +72,113 @@ def modificar_sprintplanni(request, pk, sp_pk):
         FormularioUserStory = primerpasoplanificarSprint(request.GET)
         return render(request, 'create_us.html', {'form': sprin_form, 'Proyecto': Proyecto.objects.get(pk= pk), 'permisos': permisos})
 
+'''
+def asignarus(request, pk, sp_pk):
+    # Ver si es un miembro del proyecto
+    if Miembro.objects.filter(user=request.user.id):
+        # obtener su usuario
+        user = Miembro.objects.get(rol__project_id=pk, user=request.user.id)
+    else:
+        # si no es miembro se analizan los permisos de sistema
+        user = request.user
+    # obtener sus permisos
+    permisos = user.rol.list_permissions().order_by('id')
+    #Obtener los us de aquellos que no han sido finalizado
+    uss = Us.objects.filter(project_id=pk).exclude(estado=4)
+    # Optener sprint de la cual se va a planificar
+    sprint = Sprint.objects.get(id=sp_pk)
+    # Optener formulario un sprint ya existente antes de comenzar
+    sprint_form = tercerpasoplanificarSprint(instance=sprint)
+    if request.method == 'POST':
+        # Obtener formulario cargado
+        FormularioSprint = tercerpasoplanificarSprint(request.POST)
+        # Obtener el modelo del formulario
+        nuevosp = FormularioSprint.save(commit=False)
+'''
+class asignarUs(UpdateView):
+    #Obtener la clase sprint
+    model = Sprint
+    #Validar mi formulario
+    form_class = tercerpasoplanificarSprint
+    #Agregar el html
+    template_name = 'sptercerpaso.html'
 
+    pk_sched_kwargs = 'sp_pk'  # Definir el nombre del parametro obtenido en la url
+    # Deteción de error
+    def get_object(self, queryset=None):
+        id = int(self.kwargs.get(self.pk_sched_kwargs, None))
+        obj = get_object_or_404(Sprint, pk=id)
+        return obj
+    # Validación de la url
+    def get_success_url(self):
+        proj_id = self.kwargs['pk']
+        return reverse_lazy('sprintlist', kwargs={'pk': proj_id})
+    # Metodo para filtar los permisos selecionado y el id del proyecto que se esta trabajando
+    def get_context_data(self, **kwargs):
+        context = super(asignarUs, self).get_context_data(**kwargs)
+        #Obtemos el id de nuestro proyecto
+        context['Proyecto'] = Proyecto.objects.get(pk=self.kwargs['pk'])
+        # Ver si es un miembro del proyecto
+        if Miembro.objects.filter(user=self.request.user.id):
+            # obtener su usuario
+            user = Miembro.objects.get(rol__project_id=self.kwargs['pk'], user=self.request.user.id)
+        else:
+            # si no es miembro se analizan los permisos de sistema
+            user = self.request.user
+        # obtener sus permisos
+        permisos = user.rol.list_permissions().order_by('id')
+        context['permisos'] = permisos
+        return context
 
-
-
+def listarus(request, pk, sp_pk):
+    # Ver si es un miembro del proyecto
+    if Miembro.objects.filter(user=request.user.id):
+        # obtener su usuario
+        user = Miembro.objects.get(rol__project_id=pk, user=request.user.id)
+    else:
+        # si no es miembro se analizan los permisos de sistema
+        user = request.user
+    # obtener sus permisos
+    permisos = user.rol.list_permissions().order_by('id')
+    # Obtenemos el modelo de nuestro sprint
+    sprint = Sprint.objects.get(id=sp_pk)
+    #lista de US selecionado
+    us_list=Us.objects.filter(project_id=pk, sprint__id=sp_pk)
+    #cal=0
+    #for us in us_list:
+     #   cal= cal + us.storypoints
+    #equi= Equipo.objects.get(sprint__id=sp_pk)
+    error =None
+    #if equi.capacidad < cal:
+    #    error='Capacida superada'
+    return render(request, 'listausplanificar.html', {'error': error, 'uslista': us_list,'Sprint': Sprint.objects.get(id=sp_pk), 'Proyecto': Proyecto.objects.get(pk= pk), 'permisos': permisos})
+def planificarus(request, pk, sp_pk, us_pk):
+    if Miembro.objects.filter(user=request.user.id):
+        # obtener su usuario
+        user = Miembro.objects.get(rol__project_id=pk, user=request.user.id)
+    else:
+        # si no es miembro se analizan los permisos de sistema
+        user = request.user
+    # obtener sus permisos
+    permisos = user.rol.list_permissions().order_by('id')
+    # Optener sprint de la cual se va a planificar
+    uss = Us.objects.get(id=us_pk)
+    # Optener formulario un sprint ya existente antes de comenzar
+    us_form = PlanificarUs(instance=uss)
+    if request.method == 'POST':
+        # Obtener formulario cargado
+        Formularious = PlanificarUs(request.POST)
+        # Obtener el modelo del formulario
+        if Formularious.is_valid():
+            nuevosp = Formularious.save(commit=False)
+            uss.user= nuevosp.user
+            uss.name= nuevosp.name
+            uss.storypoints= nuevosp.storypoints
+            uss.descripcion= nuevosp.descripcion
+            uss.save()
+            return redirect('listarus',pk=pk, sp_pk=sp_pk )
+        return render(request, 'planificarus.html',
+                      {'form': us_form, 'Proyecto': Proyecto.objects.get(pk=pk), 'permisos': permisos,'Sprint': Sprint.objects.get(id=sp_pk)})
+    else:
+        # Ver si es un miembro del proyecto
+        return render(request, 'planificarus.html', {'form': us_form, 'Proyecto': Proyecto.objects.get(pk=pk), 'permisos': permisos, 'Sprint': Sprint.objects.get(id=sp_pk)})
