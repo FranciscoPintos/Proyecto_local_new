e4af4ebe93d1de85d4969b59a25c1ce2b044ab5c
franpintos <pintos98javier99@fpuna.edu.py>
2021-12-02 20:18:56 -0300

    reportes_listados corregidos

--- reportes/urls.py
+++ reportes/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('reportes/proyectos', Proyectos_view.as_view(), name='listProject'),
     path('reportes/proyectos/<int:pk>/PrdocutBacklog/', ProductBacklog_Reporte_view.as_view(), name='listus'),
     path('reportes/Sprint/', Sprint_view.as_view(), name='listsprint'),
+    path('reportes/Sprint/us', us_view.as_view(), name='listus_3'),
+
     path('reportes/proyectos/<int:pk>/sprint/<int:sp_pk>/backlog/', SprintBacklog_Reporte_view.as_view(), name='listsus'),
+
+    path('reportes/proyectos/<int:pk>/us/', Us_Reporte_view.as_view(), name='us_3'),
 
 ]
--- reportes/views.py
+++ reportes/views.py
@@ -4,12 +4,13 @@ from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View, TemplateView
-from django.shortcuts import render, HttpResponse, get_object_or_404,redirect
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
 from reportes.forms import *
 from miembros.models import *
+from tarea.models import *
 
 
 # Create your views here.
@@ -27,9 +28,9 @@ class Proyectos_view(ListView):
     def get_context_data(self, **kwargs):
         context = super(Proyectos_view, self).get_context_data(**kwargs)
         FormularioProyecto = Proyecto_forms()
-        #FormularioProyecto.fields["proyectos"].queryset = Proyecto.objects.all()
+        # FormularioProyecto.fields["proyectos"].queryset = Proyecto.objects.all()
 
-        context['form'] =FormularioProyecto
+        context['form'] = FormularioProyecto
         return context
 
     def post(self, request, *args, **kwargs):
@@ -37,7 +38,7 @@ class Proyectos_view(ListView):
         # self.object = self.get_object
         form = Proyecto_forms(self.request.POST)
 
-        return redirect('listus',self.request.POST['proyectos'])
+        return redirect('listus', self.request.POST['proyectos'])
 
 
 class ProductBacklog_Reporte_view(ListView):
@@ -57,11 +58,13 @@ class ProductBacklog_Reporte_view(ListView):
         context['Proyecto'] = project
         print(context)
         return context
+
+
 def listar_us(pk):
     us = Us.objects.filter(project=pk.id)
-    list=[]
+    list = []
     for i in us:
-        list.append({'Nombre': i.name,'Estado':i.get_estado()})
+        list.append({'Nombre': i.name, 'Estado': i.get_estado()})
     return list
 
 
@@ -72,28 +75,26 @@ class Sprint_view(ListView):
     def get_context_data(self, **kwargs):
         context = super(Sprint_view, self).get_context_data(**kwargs)
         FormularioProyecto = Proyecto_forms()
-        context['form'] =FormularioProyecto
+        context['form'] = FormularioProyecto
         return context
 
     def post(self, request, *args, **kwargs):
         if request.is_ajax():
-            data={}
+            data = {}
             try:
-                data=[]
+                data = []
                 for i in Sprint.objects.filter(proyecto_id=request.POST['pk']):
-                    data.append({'id':i.id,'Nombre':i.name})
+                    data.append({'id': i.id, 'Nombre': i.name})
                 print(data)
             except Exception as e:
-                data['error']=str(e)
-            return JsonResponse(data,safe=False)
+                data['error'] = str(e)
+            return JsonResponse(data, safe=False)
         else:
             print(request.POST)
-        return redirect('listsus',request.POST['proyectos'],request.POST['Sprints'])
-
+        return redirect('listsus', request.POST['proyectos'], request.POST['Sprints'])
 
 
 class SprintBacklog_Reporte_view(TemplateView):
-
     template_name = 'SprintBackolog_reporte.html'
 
     # def get_success_url(self):
@@ -103,15 +104,52 @@ class SprintBacklog_Reporte_view(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        sprint=Sprint.objects.get(id=self.kwargs['sp_pk'],proyecto_id=self.kwargs['pk'])
-        us=sprint.us.all()
-        context['sprint']=sprint
-        context['us']=us
+        sprint = Sprint.objects.get(id=self.kwargs['sp_pk'], proyecto_id=self.kwargs['pk'])
+        us = sprint.us.all()
+
+        tarea = Tarea.objects.filter(sprimt=self.kwargs['sp_pk'])
+
+        list = []
+        for i in us:
+            suma = 0
+            for t in Tarea.objects.filter(sprimt_id=self.kwargs['sp_pk'], ustory_id=i.id):
+                suma += t.horas
+            list.append({'Nombre': i.name, 'Estado': i.get_estado(), 'horas': suma, 'storypoint': i.storypoints})
+
+        context['list'] = list
+        context['sprint'] = sprint
+        context['us'] = us
         print(context)
         return context
 
-class Us_Reporte_view(TemplateView):
 
+class us_view(ListView):
+    model = Sprint
+    template_name = 'listar_sprint_3.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        FormularioProyecto = Proyecto_forms()
+        context['form'] = FormularioProyecto
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if request.is_ajax():
+            data = {}
+            try:
+                data = []
+                for i in Sprint.objects.filter(proyecto_id=request.POST['pk']):
+                    data.append({'id': i.id, 'Nombre': i.name})
+                print(data)
+            except Exception as e:
+                data['error'] = str(e)
+            return JsonResponse(data, safe=False)
+        else:
+            print(request.POST)
+        return redirect('us_3', request.POST['proyectos'])
+
+
+class Us_Reporte_view(TemplateView):
     template_name = 'Us_reporte.html'
 
     # def get_success_url(self):
@@ -121,9 +159,21 @@ class Us_Reporte_view(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        sprint=Sprint.objects.get(id=self.kwargs['sp_pk'],proyecto_id=self.kwargs['pk'])
-        us=sprint.us.all()
-        context['sprint']=sprint
-        context['us']=us
+        sprint = Sprint.objects.get(proyecto_id=self.kwargs['pk'], estado=2)
+        us = sprint.us.all()
+
+        list = []
+        for i in us:
+            suma = 0
+            for t in Tarea.objects.filter(sprimt_id=sprint.id, ustory_id=i.id):
+                suma += t.horas
+
+            list.append({'Nombre': i.name, 'Estado': i.get_estado(), 'horas': suma, 'user': i.user})
+
+
+
+        context['list'] = list
+        context['sprint'] = sprint
+        context['us'] = us
         print(context)
         return context
