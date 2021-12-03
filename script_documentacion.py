f9649a30e0fceaec5880f3749a5a31e65a563518
elias <eliascaceres124@fpuna.edu.py>
2021-12-03 01:16:45 -0300

    script_documentacion modificado y cambiado sus permisos

--- script_documentacion.py
+++ script_documentacion.py
@@ -1,11 +1,37 @@
 #aqui definimos la ruta del intérprete de python
 #!/usr/bin/python3
 
+
+echo 'Iniciando script de documentacion...'
+for i in {1..2}; do
+    sleep 1;
+done
+
 #para poder ejecutar la documentación debemos retroceder una rama
 cd $'..'
+
+echo 'Borrando documentacion anterior...'
+
+for i in {1..2}; do
+    sleep 1;
+done
+
+fichero='docs'
+
+rm -r $fichero
+
+echo 'creando nueva documentacion...'
+
+for i in {1..2}; do
+    sleep 1;
+done
+
 #podemos ejecutar el comando para la documentacion automatica
 ejecutar_documentacion="Proyecto_local_new/**/*.py"
 pycco $ejecutar_documentacion -pi
 
 #una vez se haya hecho toda la documentacion volvemos a la rama donde tenemos el archivo manage.py
 cd $'Proyecto_local_new'
+
+
+echo 'Documentacion completada...'
